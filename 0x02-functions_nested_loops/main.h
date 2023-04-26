7ea081b29b58bf30cbe0d277b4753ec5fc7b229c
xclusivestar <onijumoke4@gmail.com>
2023-04-26 13:24:07 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 void print_alphabet(void);
-void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _putchar(int c);
 int _islower(int c);
@@ -11,3 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+int _isalpha(int c);
