44c189687a1c9976e129b4468acf396c57eba298
xclusivestar <onijumoke4@gmail.com>
2023-04-28 10:02:37 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,17 @@
 	int largest;
 
 	if (a > b && a > c)
-    {
+	{
 	largest = a;
-    }
+	}
 	else if (b > c)
-    {
+	{
 	largest = b;
-    }
+	}
 	else
-    {
+	{
 	largest = c;
-    }
+	}
 
 	return (largest);
 }
